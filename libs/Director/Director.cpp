1f934c1b641f0430f5c184b094acac20f2835c91
unknown <mcanelly.sam@live.com>
2019-02-14 11:58:08 -0600

    Changed main control loop to look for start and exit condition. Other minor tweaks

--- libs/Director/Director.cpp
+++ libs/Director/Director.cpp
@@ -11,27 +11,42 @@
 
 #include "Director.h"
 
-void Director::start() {
+void Director::reset() {
+    //constantly loop and look for an arm signal
+    while(active == false) {
+        //do nothing and wait
+        receiver->update();
+    }
+
+    int starting_position = 0/*pendulum_reader->getPosition()*/;
+
+    //wait for pendulum to be in an acceptable start range
+    while(!within_range(starting_position)) {
+        //delay here to wait?
+        starting_position = 0/*pendulum_reader->getPosition()*/;
+    }
+
+    loop();
+}
+
+void Director::loop() {
     //create PID controller object here
     double set_point, input, output;
     //ex:
     //PID myPID(&input, &output, &set_point,2,5,1, DIRECT);
 
-    while(true/*exit condition != true */) {
+    while(active == true) {
         receiver->update();
 
-        //set_point = convert_throttle(getThrottleValue)
+        //set_point = convert_throttle(getThrottleValue());
         //input=pendulum_reader->getPosition();
         //myPID.compute()
         //(maybe convert output here to another value)
         //(could also be handled in the motor_controller object)
         //motor_controller->set_motors(output);
-        //update exit condition value
     }
-}
 
-void Director::stop() {
-    //set exit condition variable
+    reset();
 }
 
 double Director::convert_throttle(int throttle_value) {
